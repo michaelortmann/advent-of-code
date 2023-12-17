f2963bdeda9a78bef49322508fc5d9ed35aeb5c7
Michael Ortmann <keine email>
2023-12-17 01:57:57 +0100

    Add 2023/day09

---
+++ 2023/day09/day09p1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2023 Michael Ortmann
+
+import sys
+
+def next(x):
+  return x[-1] if len({s for s in x}) == 1 else x[-1] + next([(x[i + 1] - x[i]) for i in range(0, len(x) - 1)])
+
+y = 0
+
+for x in [[int(x) for x in line.split(" ")] for line in sys.stdin.read().splitlines()]:
+  y += next(x)
+
+print(y)
